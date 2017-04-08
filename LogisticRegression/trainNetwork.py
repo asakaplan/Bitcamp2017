24ce8049a44aae4921ac53666f2e254f433f39b6
Asa Kaplan <asa.kaplan@toptal.com>
2017-04-08 14:55:14 -0400

    Added test data, generated SQL calls, ...

--- LogisticRegression/trainNetwork.py
+++ LogisticRegression/trainNetwork.py
@@ -1,11 +1,15 @@
 from sklearn import linear_model, preprocessing
 from sklearn.externals import joblib
-
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import matthews_corrcoef, confusion_matrix
+import pickle
 
 # Load Data
 def trainNetwork(inputFile = 'trainingInput.pkl',expectedOutputFile = 'trainingOutput.pkl', modelOutputFile = 'model.pkl'):
-	X = joblib.load('trainingInput.pkl')
-	Y = joblib.load('trainingOutput.pkl')
+	a=pickle.load(open("../consolodatedOutput.pic","r"))
+	X = [b[2] for b in a]
+	#X = joblib.load('trainingInput.pkl')
+	Y = joblib.load('../isBad.pic')
 
 	# Feature Scaling / Normalization
 	X = preprocessing.normalize(X, norm='l2')
@@ -13,12 +17,14 @@ def trainNetwork(inputFile = 'trainingInput.pkl',expectedOutputFile = 'trainingO
 
 	# Create Model
 	logreg = linear_model.LogisticRegression(C=1e5)
-
+	scores = cross_val_score(logreg, X, Y, cv=5)
+	print(scores)
 	logreg.fit(X, Y)
-
-
+	print X[:10]
+	print(confusion_matrix(Y,logreg.predict(X)))
+	print(matthews_corrcoef(Y,logreg.predict(X)))
 	# Save Model
-	joblib.dump(logreg, 'model.pkl') 
+	joblib.dump(logreg, 'model.pkl')
 
 	# Update Training Input
 	joblib.dump(X,'trainingInput.pkl')
--- classifier.py
+++ classifier.py
@@ -1,11 +1,19 @@
 import pickle
+from random import random
 a=pickle.load(open("consolodatedOutput.pic","r"))
 print "Loaded data"
+print a[1]
+exit(0)
 threshold = 4.0
+NOISE_BAD = 0.3
+NOISE_GOOD =0# 0.001
 weights = [1.0, 0.0, 2.0, 5.0,0.5,1.0,2.0,1.0,0.5] #['hasBankrupt', 'hasBond', 'hasCivilJudc', 'hasCriminal', 'hasCustComp', 'hasInvstgn', 'hasJudgment', 'hasRegAction', 'hasTermination']
 isBad = []
 for i in range(len(a)):
     outp = sum(weights[j]*a[i][2][j] for j in range(len(weights)))
-    isBad.append(outp>=threshold)
+    if outp>=threshold:
+        isBad.append(random()>NOISE_BAD)
+    else:
+        isBad.append(random()<NOISE_GOOD)
 print sum(isBad), len(isBad)
 pickle.dump(isBad, open("isBad.pic","w"))
