e7b499b84e47fee495b44afb3285654258058cad
Stuart Geipel <lapimlu@gmail.com>
2017-04-09 04:08:04 -0400

    setup callback for ui update

--- public/js/ui.jsx
+++ public/js/ui.jsx
@@ -1,11 +1,13 @@
-const nodeInfo = (
-  <div>
-    <h2>Some Person</h2>
-    <h3>Mopley Starnely Bank</h3>
-  </div>
-);
-
-ReactDOM.render(
-  nodeInfo,
-  document.getElementById('root')
-);
+function uiUpdate(o) {
+  const nodeInfo = (
+    <div>
+      <h2>{o.name}</h2>
+      <h3>{o.comp}</h3>
+    </div>
+  );
+  const unselected = (<div><h3>Select a node...</h3></div>);
+  ReactDOM.render(
+    o.selected ? nodeInfo : unselected,
+    document.getElementById('root')
+  );
+}
